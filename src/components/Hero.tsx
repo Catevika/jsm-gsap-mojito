28e3a27a3e00b2208ccc085085b4f0fc0b85b3ec
Catevika <dominique.bello@outlook.fr>
2025-10-22 18:14:27 +0200

    Add Hero - part 2 = scroll animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,12 @@
 import gsap from 'gsap';
 import {SplitText} from 'gsap/all';
+import {useRef} from 'react';
+import {useMediaQuery} from 'react-responsive';
 
 const Hero = () => {
+	const videoRef = useRef<HTMLVideoElement>(null);
+	const isMobile = useMediaQuery({maxWidth: 767});
+
 	document.fonts.ready.then(() => {
 		const heroSplit = new SplitText('.title', {type: 'chars, words'});
 		const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
@@ -35,6 +40,27 @@ const Hero = () => {
 			})
 			.to('.right-leaf', {y: 200}, 0)
 			.to('.left-leaf', {y: -200}, 0);
+
+		const startValue = isMobile ? 'top 50%' : 'center 60%';
+		const endValue = isMobile ? '120% top ' : 'bottom top';
+
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: 'video',
+				start: startValue,
+				end: endValue,
+				scrub: true,
+				pin: true,
+			},
+		});
+
+		if (videoRef.current) {
+			videoRef.current.onloadedmetadata = () => {
+				tl.to(videoRef.current, {
+					currentTime: videoRef.current && videoRef.current.duration,
+				});
+			};
+		}
 	});
 
 	return (
@@ -73,6 +99,16 @@ const Hero = () => {
 					</div>
 				</div>
 			</section>
+			<div className='video absolute inset-0'>
+				<video
+					ref={videoRef}
+					muted
+					playsInline
+					preload='auto'
+					loop
+					src='/videos/output.mp4'
+				/>
+			</div>
 		</>
 	);
 };
